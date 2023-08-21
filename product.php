3125d677af225e8dd2f713744ae7a8aa643c874e
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-08-21 16:06:31 +0600

    simple raw php project layout

---
+++ product.php
@@ -0,0 +1,23 @@
+<!-- Header START -->
+<!-- Header will include external style link, title, favicon, etc -->
+<?php include_once('inc/header.php'); ?>
+<!-- Header END -->
+
+<body>
+
+    <!-- Navbar (sit on top) -->
+    <?php include_once('inc/nav.php'); ?>
+    <!-- Navbar END -->
+
+    <!-- Main Content Start -->
+    <div class="content">
+
+    </div>
+    <!-- Main Content END -->
+
+    <!-- Footer Start -->
+    <?php include_once('inc/footer.php'); ?>
+    <!-- Footer END -->
+</body>
+
+</html>
